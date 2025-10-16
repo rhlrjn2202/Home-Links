cb4cc64f0c402cbcee68cbf58e5b95633d6bb0a4
[dyad] <rhlrjn2202@gmail.com>
2025-10-16 20:31:32 +0530

    [dyad] Added name and mobile number fields to user sign-up. - wrote 2 file(s), executed 1 SQL queries

--- src/app/userauth/login/page.tsx
+++ src/app/userauth/login/page.tsx
@@ -54,7 +54,6 @@ export default function UserLoginPage() {
               },
             }}
             theme="light" // Use light theme by default
-            // Removed redirectTo prop to allow SessionContextProvider to handle redirects
           />
         </div>
       </main>
